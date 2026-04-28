62e6f2f6aea479e91104d83efd33fa35d876f326
Muska Liesaho <muska.liesaho@student.hamk.fi>
2026-04-28 12:47:19 +0300

    render korjattu

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -124,8 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import os
 
 
@@ -137,12 +140,14 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",  
+    "https://valokuvagalleria-frontend.onrender.com",
 ]
 
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://valokuvagalleria-frontend.onrender.com", 
 ]
 
 
